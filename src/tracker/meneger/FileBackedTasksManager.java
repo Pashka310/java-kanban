88a4bbc11f49703880d748768f1815e5803d2928
Pavel Bayok <pashka310@yandex.by>
2024-03-06 02:29:19 +0300

    6_3 спринт test7

--- src/tracker/meneger/FileBackedTasksManager.java
+++ src/tracker/meneger/FileBackedTasksManager.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class FileBackedTasksManager extends InMemoryTaskManager{
+public class FileBackedTasksManager extends InMemoryTaskManager {
 
     public static void main(String[] args) {
 
