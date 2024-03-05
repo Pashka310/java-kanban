b85a67e8b289baaae61201ba1c9f4265ad8203e7
Pavel Bayok <pashka310@yandex.by>
2024-03-06 02:27:01 +0300

    6_3 спринт test6

--- src/tracker/meneger/Managers.java
+++ src/tracker/meneger/Managers.java
@@ -4,15 +4,15 @@ import java.io.File;
 
 public class Managers {
 
-    public static TaskManager getDefault(){
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
 
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return  new InMemoryHistoryManager();
     }
 
-    public static TaskManager getDefaultFile(){
+    public static TaskManager getDefaultFile() {
         return new FileBackedTasksManager(new File("src/tracker/files/historyfile.csv"));
     }
 }
